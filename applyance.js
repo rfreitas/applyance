1c1f14c96113631403d28a520322059419ed8664
Ricardo Freitas <derfreitas@gmail.com>
2012-11-26 10:07:44 +0000

    added consistency and an api that matches javascript programmers expectations

--- applyance.js
+++ applyance.js
@@ -34,9 +34,9 @@ http://stackoverflow.com/questions/7991607/best-publish-subscribe-library-in-jav
 		var delegator = function(){
 			var meta = delegator.delegatorMeta;
 
-			out.forEachApply( meta.delegatesBefore , arguments );
+			out.forEachApply( meta.delegatesBefore , arguments, this );
 			var result = typeof meta.wrappedFunction === "function" ? meta.wrappedFunction.apply(this, arguments) : undefined;
-			out.forEachApply( meta.delegates , arguments );
+			out.forEachApply( meta.delegates , arguments, this );
 
 			return result;
 		};
@@ -63,69 +63,72 @@ http://stackoverflow.com/questions/7991607/best-publish-subscribe-library-in-jav
 
 
 		if ( force || typeof existingFunc === "function" || typeof existingFunc === "undefined" || existingFunc === null){
-			newFunc = out.createDelegator();
+			newFunc = out.createDelegator( existingFunc );
 		}
 		else{
-		//error
-		return;
-	}
-
-
-	if (typeof existingFunc === "function"){
-		newFunc.delegatorMeta.wrappedFunction = function(){
-			existingFunc.apply(delegatorObject, arguments);
-		};
-	}
-	
-	/* the user chooses what to do
-	if ( Object.freeze ){
-		Object.freeze(delegatorMeta);
-		Object.freeze(newFunc);
-	}
-	*/
-
-	delegatorObject[function_key] = newFunc;
-
-	return newFunc;
-};
-
-
-out.tearDown = function( delegator, function_key ){
-	var func = delegator[function_key];
-	if ( out.isDelegator(func) ){
-		var existingFunc = func.delegatorMeta.wrappedFunction;
-		if(existingFunc){
-			delegator[function_key] = existingFunc;
+			//error
+			return;
 		}
-		return true;
-	}
-	return false;
-};
 
-out.isDelegator = function(func){
-	return typeof func === "function" && typeof func.delegatorMeta === "object";
-};
-
-//before and target are both optional
-out.delegate = function( delegator, function_key, handler, before, target  ){
-	//handler prep
-	if (target){
-		handler = function(){
-			return handler.apply(target,arguments);
-		};
-	}
+		
+		delegatorObject.__defineGetter__( function_key, function(){
+			return newFunc;
+		});
 	
-	var funcToDelegate = out.setUp( delegator, function_key );
+		
+		delegatorObject.__defineSetter__( function_key , function(newVal){
+			var prop = delegatorObject[function_key];
+			if ( out.isDelegator(prop) &&  typeof newVal === "function"){
+				prop.delegatorMeta.wrappedFunction = newVal;
+			}
+			else{
+				delete this[function_key];
+				this[function_key] = newVal;
+			}
+			
+		});
 
-	if ( typeof funcToDelegate !== "function"){
-		console.log("Function to delegate from is not really a function but a "+typeof funcToDelegate);
+		return newFunc;
+	};
+
+
+	out.tearDown = function( delegator, function_key ){
+		var func = delegator[function_key];
+		if ( out.isDelegator(func) ){
+			var existingFunc = func.delegatorMeta.wrappedFunction;
+			delete delegator[function_key];
+			if(existingFunc){
+				delegator[function_key] = existingFunc;
+			}
+			return true;
+		}
 		return false;
-	}
+	};
 
-	var delegatesArray = before ? funcToDelegate.delegatorMeta.delegatesBefore : funcToDelegate.delegatorMeta.delegates;
+	out.isDelegator = function(func){
+		return typeof func === "function" && typeof func.delegatorMeta === "object";
+	};
 
-	delegatesArray.push( handler );
-	return funcToDelegate;
-};
+	//before and target are both optional
+	out.delegate = function( delegator, function_key, handler, before, target  ){
+		//handler prep
+		if (target){
+			handler = function(){
+				return handler.apply(target,arguments);
+			};
+		}
+		
+		var funcToDelegate = out.setUp( delegator, function_key );
+
+		if ( typeof funcToDelegate !== "function"){
+			console.log("Function to delegate from is not really a function but a "+typeof funcToDelegate);
+			return false;
+		}
+
+		var delegatesArray = before ? funcToDelegate.delegatorMeta.delegatesBefore : funcToDelegate.delegatorMeta.delegates;
+
+		delegatesArray.push( handler );
+		return funcToDelegate;
+	};
 
 })(window, undefined);
